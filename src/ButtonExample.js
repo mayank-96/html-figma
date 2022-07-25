1c586ce96d80a99da757e1f5dea1be96aeb53385
mayank-96 <pagarmayank07@gmail.com>
2022-07-25 12:29:06 +0530

    feat: added green button

---
+++ src/ButtonExample.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Button } from "native-base";
+
+function ButtonExample() {
+  return (
+    <Box alignItems="center" h="100vh" justifyContent="center" bg="gray.100">
+      <Button onPress={() => console.log("hello world")} bg="green.500">
+        Click Me
+      </Button>
+    </Box>
+  );
+}
+
+export default ButtonExample;
