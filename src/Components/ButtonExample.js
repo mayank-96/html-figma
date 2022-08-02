ab304a809f8f68f06b23ac6627832e013051fc61
mayank-96 <pagarmayank07@gmail.com>
2022-08-02 19:25:46 +0530

    ff

--- src/Components/ButtonExample.js
+++ src/Components/ButtonExample.js
@@ -3,7 +3,13 @@ import { Box, Button } from "native-base";
 
 function ButtonExample() {
   return (
-    <Box alignItems="center" h="100vh" justifyContent="center" bg="gray.100">
+    <Box
+      className="button-nb"
+      alignItems="center"
+      h="100vh"
+      justifyContent="center"
+      bg="gray.100"
+    >
       <Button onPress={() => console.log("hello world")} bg="green.500">
         Click Me
       </Button>
